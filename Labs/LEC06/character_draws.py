3440e782bafef6d1dc79921706729df4949dae9b
Pkhk_woorek <wooreknaver@gmail.com>
2026-09-22 19:43:37 +0900

    .
    .

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,17 +1,10 @@
 from pico2d import *
 
 open_canvas(800, 600)
+boy = load_image('character.png')
 
-def move_circle():
-    print('circle')
-
-def move_rectangle():
-    print('rectangle')
-
-def move_triangle():
-    print('triangle')
-
-while True:
-    move_circle()
-    move_rectangle()
-    move_triangle()
+clear_canvas()
+boy.draw(400, 300)
+update_canvas()
+delay(1)
+close_canvas()
