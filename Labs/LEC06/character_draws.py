571f75005359235d2c749cd1f96c151e513abf5e
Pkhk_woorek <wooreknaver@gmail.com>
2026-09-22 19:15:54 +0900

    .
    .

---
+++ Labs/LEC06/character_draws.py
@@ -0,0 +1,13 @@
+def move_circle():
+    print('circle')
+
+def move_rectangle():
+    print('rectangle')
+
+def move_triangle():
+    print('triangle')
+
+while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
