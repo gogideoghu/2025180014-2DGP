98093ce8e67f8fd1cb9fed4c5c0c1ff1dcf3209c
Pkhk_woorek <wooreknaver@gmail.com>
2026-09-22 19:49:36 +0900

    character이미지 불러오기
    파일 위치나 경로변경, 파이썬 버전 변경등을 진행해도 이미지가 불러와지지 않는 문제가 발생하여 ai를 이용하여 해당 부분을 수정하였습니다.

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,10 +1,29 @@
+from os.path import abspath, dirname, join
+
 from pico2d import *
 
 open_canvas(800, 600)
-boy = load_image('character.png')
+image_path = join(dirname(abspath(__file__)), 'character.png')
+boy = load_image(image_path)
+
+#어떤 이유에서인지 character.png파일이 제대로 불러와지지 않아 해당 부분은 ai를 통해 수정하였습니다.
 
 clear_canvas()
 boy.draw(400, 300)
 update_canvas()
 delay(1)
 close_canvas()
+
+def move_circle():
+    pass
+
+def move_rectangle():
+    pass
+
+def move_triangle():
+    pass
+
+while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
