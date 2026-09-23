8617446265e402a8e572678f516f408f5db296fe
Pkhk_woorek <wooreknaver@gmail.com>
2026-09-23 11:42:24 +0900

    .
    .

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -2,10 +2,12 @@ from os.path import abspath, dirname, join
 import math
 from pico2d import *
 
-open_canvas(800, 600)
-#어떤 이유에서인지 character.png파일이 제대로 불러와지지 않아 해당 부분은 ai를 통해 수정하였습니다.
-image_path = join(dirname(abspath(__file__)), 'character.png')
-boy = load_image(image_path)
+
+def draw_boy(x, y):
+    clear_canvas()
+    boy.draw(x, y)
+    update_canvas()
+    delay(0.01)
 
 degree = 0
 theta = math.radians(degree)
